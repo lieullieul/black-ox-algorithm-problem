d43523de4e11402ccb6c0b0a1ee5be95968f3cdb
Kanich-i <sexyiori@naver.com>
2022-10-27 12:43:46 +0900

    Update: 60057.cpp - 효율성 개선

--- Programers/C++/60057.cpp
+++ Programers/C++/60057.cpp
@@ -5,20 +5,19 @@ using namespace std;
 
 int solution(string s)
 {
-    vector<string> vecCompStr;  // 압축된 문자열 벡터
-    string strCnt;              // 숫자 -> 문자열
+    vector<int> vecAnswer;      // 압축된 문자열 길이 벡터
     string standard;            // 반복 확인할 기준 문자열
-    string tmp;                 // vecCompStr에 저장할 임시 문자열
     size_t i;
     size_t cnt;                 // 기준 문자열이 몇 번 반복됐는지 체크할 변수
     size_t size = s.size();
+    int tmpSize;
     int answer = s.size();
 
     // stdDist = 기준 문자열의 길이
     // stdDist 1부터 s의 길이 반까지 늘려가며 탐색
     for (size_t stdDist = 1; stdDist <= size / 2; stdDist++)
     {
-        tmp = "";
+        tmpSize = 0;
         i = 0;
         while (i < size)
         {
@@ -36,26 +35,29 @@ int solution(string s)
             // 2회 이상 반복 시 숫자 처리
             if (cnt > 1)
             {
-                strCnt = "";
                 while (cnt > 0)
                 {
-                    strCnt = static_cast<char>(cnt % 10 + '0') + strCnt;
+                    tmpSize++;
                     cnt /= 10;
                 }
-                tmp += strCnt;
             }
-            tmp += standard;
+            tmpSize += standard.size();
         }
 
         // stdDist 단위로 잘라냈을 때의 결과 저장
-        vecCompStr.push_back(tmp);
+        vecAnswer.push_back(tmpSize);
     }
 
     // 가장 짧은 문자열 탐색
-    for (int i = 0; i < vecCompStr.size(); i++)
+    for (int i = 0; i < vecAnswer.size(); i++)
     {
-        if (vecCompStr[i].size() < answer)
-            answer = vecCompStr[i].size();
+        if (vecAnswer[i] < answer)
+            answer = vecAnswer[i];
     }
     return answer;
+}
+
+int main()
+{
+    solution("abcabcdede");
 }
