41d8fd1ab23947cf942e4cbb5a971921ad4c8a00
Kanich-i <soulstr89@gmail.com>
2022-11-14 18:02:58 +0900

    Update 42584.c

--- Programers/C/42584.c
+++ Programers/C/42584.c
@@ -3,26 +3,29 @@
 // 인덱스 스택
 int g_stackIdx[100000];
 
-// 배열 전체 탐색
-// 현재 값이 탑보다 작으면 팝
-//  -> 현재 위치와 탑과의 거리 저장
-// 현재 값 푸시
 // O(n) 탐색 + O(n-1) 팝
 int* solution(int prices[], size_t prices_len)
 {
     int* answer = (int*)malloc(sizeof(int) * prices_len);
     int top = 0;
 
+    // 배열 전체 탐색
+    // 첫 인덱스는 무조건 0이므로 생략
     for (size_t i = 1; i + 1 < prices_len; i++)
     {
+        // 현재 값이 탑보다 작으면 팝
+        //  -> 현재 위치와 탑과의 거리 저장
         while (top >= 0 && prices[g_stackIdx[top]] > prices[i])
         {
             answer[g_stackIdx[top]] = i - g_stackIdx[top];
             top--;
         }
+        // 현재 인덱스 푸시
         top++;
         g_stackIdx[top] = i;
     }
+
+    // 스택 정리
     while (top >= 0)
     {
         answer[g_stackIdx[top]] = (prices_len - 1) - g_stackIdx[top];
