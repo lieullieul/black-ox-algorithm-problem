4a96d73023d33e01d8b76e63a82646535a28cfa7
Kanich-i <soulstr89@gmail.com>
2023-01-21 01:19:58 +0900

    Update 148652.c

--- Programers/C/148652.c
+++ Programers/C/148652.c
@@ -2,18 +2,9 @@
 
 int isOneRecursive(int n, long long pos)
 {
-    long long areaSize;
-    long long area3Left;
-    long long area3Right;
-
     if (n == 1)
         return pos != 3;
     if ((pos + 4) % 5 == 2)
-        return 0;
-    areaSize = (long long)pow(5, n - 1);
-    area3Left = areaSize * 2 + 1;
-    area3Right = areaSize * 3;
-    if (area3Left <= pos && pos <= area3Right)
         return 0;
     long long nextPos = (pos + 4) / 5;
     return isOneRecursive(n - 1, nextPos);
