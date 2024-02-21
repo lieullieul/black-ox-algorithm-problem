6ed9eeb5d5714af834c487c1b5cd2ba7572ef748
Kanich-i <soulstr89@gmail.com>
2024-02-21 14:58:12 +0900

    Create 201_Bitwise_AND_of_Numbers_Range.cpp

---
+++ Leetcode/C++/201_Bitwise_AND_of_Numbers_Range.cpp
@@ -0,0 +1,17 @@
+// 201. 201 Bitwise AND of Numbers Range
+// https://leetcode.com/problems/bitwise-and-of-numbers-range/
+
+class Solution {
+public:
+    int rangeBitwiseAnd(int left, int right) {
+        int cnt = 0;
+
+        while (left != right)
+        {
+            left >>= 1;
+            right >>= 1;
+            cnt++;
+        }
+        return left << cnt;
+    }
+};
